f4d3daee831f029703c56154abedefde52e52444
TEJAS V. MAHAJAN <mahajantejasv@gmail.com>
2021-05-05 17:53:52 +0530

    Create singly-linked-list.ts

---
+++ linkedList/singly-linked-list/singly-linked-list.ts
@@ -0,0 +1,83 @@
+class SinglyLinkedList {
+  head: SinglyLinkedListNode = null;
+  createSinglyLinkedList(data: any[]) {
+    for (let index = 0; index < data.length; index++) {
+      this.addNode(data[index]);
+    }
+  }
+
+  printSinglyLinkedList() {
+    //  to print entire model uncomment line mentioned below
+    //  console.log(this.head);
+    let currentNode = { ...this.head };
+    while (currentNode != null) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
+    }
+  }
+
+  addNode(value) {
+    if (this.head != null) {
+      let currentNode = this.head;
+      while (currentNode.next != null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = { ...this.getNewNode(value) };
+    } else {
+      this.head = { ...this.getNewNode(value) };
+    }
+  }
+
+  getNewNode(value): SinglyLinkedListNode {
+    let singlyLinkedListNode = new SinglyLinkedListNode();
+    singlyLinkedListNode.value = value;
+    singlyLinkedListNode.next = null;
+    return singlyLinkedListNode;
+  }
+
+  deleteNode(value) {
+    if (this.head) {
+      let currentNode = this.head;
+      if (currentNode.value === value) this.head = this.head.next;
+      else {
+        while (currentNode.next.value != value) {
+          currentNode = currentNode.next;
+        }
+        currentNode.next = currentNode.next.next;
+      }
+    }
+  }
+
+  calculateLength(node: SinglyLinkedListNode) {
+    return node ? 1 + this.calculateLength(node.next) : 0;
+  }
+
+  search(node: SinglyLinkedListNode, value): SinglyLinkedListNode {
+    if (node) {
+      return node.value != value ? this.search(node.next, value) : node;
+    } else {
+      return null;
+    }
+  }
+}
+
+class SinglyLinkedListNode {
+  value: any;
+  next: SinglyLinkedListNode;
+}
+
+let singlyLinkedListInstance = new SinglyLinkedList();
+singlyLinkedListInstance.createSinglyLinkedList([1, 2, 3, 4]);
+console.log("Singly Linked List :::: List Has Been Created :::: ");
+singlyLinkedListInstance.printSinglyLinkedList();
+singlyLinkedListInstance.addNode("New Node Data");
+console.log("Singly Linked List :::: New Node has Been Created :::: ");
+singlyLinkedListInstance.printSinglyLinkedList();
+singlyLinkedListInstance.deleteNode(3);
+console.log("Singly Linked List :::: Node has Been Deleted :::: ");
+singlyLinkedListInstance.printSinglyLinkedList();
+console.log("Singly Linked List :::: Length Calculation :::: ");
+console.log(singlyLinkedListInstance.calculateLength(singlyLinkedListInstance.head));
+console.log("Singly Linked List :::: Search :::: ");
+console.log(singlyLinkedListInstance.search(singlyLinkedListInstance.head, 4) ?
+  "Found" : "Not Found");
